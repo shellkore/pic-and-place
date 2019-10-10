32cb4b1510035a27cc2a453b43b0b213f11444ac
shellkore <shellkoresahu@gmail.com>
2019-10-11 05:01:23 +0530

    create name folder and place photo

--- faceRecog.py
+++ faceRecog.py
@@ -21,6 +21,8 @@ def loadKnownImage(knownDirecName):
 	for known_image in known_image_list:
 		known_faces.append(face_recognition.face_encodings(known_image)[0])
 
+	return(fileList)
+
 def loadAndCheck(unknownImageList):
 	row = len(unknownImageList)
 	col = len(known_image_list)
@@ -35,30 +37,5 @@ def loadAndCheck(unknownImageList):
 				if(results[j]):
 					found_faces[i][j] = True
 	os.chdir("..")
-	return (found_faces)
-'''
-try:
-	biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-	obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-	obamaji_face_encoding = face_recognition.face_encodings(obamaji_image)[0]
-	unknown_face_encoding = face_recognition.face_encodings(unknown_image)
-except IndexError:
-    print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
-    quit()
+	return (found_faces,row,col)
 
-known_faces = [
-    biden_face_encoding,
-    obama_face_encoding,
-	obamaji_face_encoding
-]
-
-# results is an array of True/False telling if the unknown face matched anyone in the known_faces array
-for face in unknown_face_encoding:
-		
-	results = face_recognition.compare_faces(known_faces, face, 0.54)
-
-	print("Is the unknown face a picture of Shailesh? {}".format(results[0]))
-	print("Is the unknown face a picture of Shubham? {}".format(results[1]))
-	print("Is the unknown face a picture of Shivam? {}".format(results[2]))
-	print("Is the unknown face a new person that we've never seen before? {}".format(not True in results))
-'''
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import faceRecog
+from shutil import copy2
 
 owd = os.getcwd()
 '''
@@ -7,14 +8,40 @@ with open('details.json') as json_file:
 		detailDict = json.load(json_file)
 '''
 
-faceRecog.loadKnownImage("known")
+knownImageList = faceRecog.loadKnownImage("known")
 
 for (root,direc,file) in os.walk("unknown"):
 		unknownImageList = file
 
+def createAndPlace(imageName,faceName):
+	print(imageName,faceName)
+	folderName = faceName[:-4]
+	if not os.path.exists(folderName):
+	    os.makedirs(folderName)
 
-foundFaceMatrix = faceRecog.loadAndCheck(unknownImageList)
-print(foundFaceMatrix)
+	src_path = os.getcwd()+'/unknown/'+imageName
+	dest_path = os.getcwd()+'/'+folderName+'/'
+	print(src_path)
+	print(dest_path)
+
+	copy2(src_path,dest_path)
+
+
+foundFace= faceRecog.loadAndCheck(unknownImageList)
+print(foundFace)
+
+foundFaceMatrix = foundFace[0]
+row = foundFace[1]
+col = foundFace[2]
+
+for i in range(row):
+	for j in range(col):
+		if(foundFaceMatrix[i][j]):
+			createAndPlace(unknownImageList[i],knownImageList[j])
+
+
+
+
 
 '''
 for unknownImage in unknownImageList:
