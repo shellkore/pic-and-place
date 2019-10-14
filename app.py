c6fc486ea055d6479c136dd628866fd39cc338a3
shellkore <shellkoresahu@gmail.com>
2019-10-14 15:09:31 +0530

    mail collection in flask app

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import json
 import os
 
 
+with open('mailID.json') as json_file:
+		mailID = json.load(json_file)
+
+print(mailID)
+
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
@@ -15,13 +20,17 @@ def dataOfImage():
 	imgstring=request.form['data']
 	name = request.form['name']
 	email= request.form['email']
-	print(name,email)
+	mailID[name]=email
+	print ("after app:",mailID)
+	with open('mailID.json','w') as json_file:
+		json.dump(mailID,json_file)
 	imgdata = base64.b64decode(imgstring)
 	filename = name+'.jpg'
 	os.chdir("known")
 	with open(filename, 'wb') as f:
 	 f.write(imgdata)
 	os.chdir("..")
+	
   	
   
 if __name__ == '__main__':
